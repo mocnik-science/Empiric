af31de62a32406025c4ca5c6b6dacd6395bd133e
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2020-05-09 06:34:13 +0200

    code cleanup

--- Empiric/internal/ManuscriptMemory.py
+++ Empiric/internal/ManuscriptMemory.py
@@ -18,8 +18,8 @@ class ManuscriptMemory():
   def __init__(self, accessCode):
     self._stepCounter = 0
     self._data = {
-      'log': {},
       'statistics': {},
+      'memory': {},
       'metadata': {
         'accessCode': accessCode,
         'timestamp': self._currentTimestamp(),
@@ -35,20 +35,20 @@ class ManuscriptMemory():
     if not os.path.exists(ManuscriptMemory._pathCollectedData):
       os.makedirs(ManuscriptMemory._pathCollectedData)
     tmp = self._getMetadata('timestamp').replace(':', '-') if self.isDefaultAccessCode() else self.accessCode()
-  def _logEmpty(self):
-    return self._data['log'] == {}
-  def _stepInLog(self, step):
-    return str(step) in self._data['log']
     return os.path.join(ManuscriptMemory._pathCollectedData, f'experiment-{tmp}.json')
+  def _memoryEmpty(self):
+    return self._data['memory'] == {}
+  def _stepInMemory(self, step):
+    return str(step) in self._data['memory']
   def _initStep(self, step):
-    self._data['log'][str(step)] = {}
-  def __getLogWithoutKey(self, step):
-    return self._data['log'][str(step)] if self._stepInLog(step) else None
-  def _getLog(self, step, key):
-    m = self.__getLogWithoutKey(step)
+    self._data['memory'][str(step)] = {}
+  def __getMemoryWithoutKey(self, step):
+    return self._data['memory'][str(step)] if self._stepInMemory(step) else None
+  def _getMemory(self, step, key):
+    m = self.__getMemoryWithoutKey(step)
     return m[key] if key in m else None
-  def _setLog(self, step, key, value=None, defaultValue=None):
-    m = self.__getLogWithoutKey(step)
+  def _setMemory(self, step, key, value=None, defaultValue=None):
+    m = self.__getMemoryWithoutKey(step)
     if key in m:
       if value is not None:
         m[key] = value
@@ -65,18 +65,18 @@ class ManuscriptMemory():
     return self._data['metadata'][key] if key in self._data['metadata'] else None
   def prepareRun(self):
     self._stepCounter = 0
-    if not self.isDefaultAccessCode() and self._logEmpty():
+    if not self.isDefaultAccessCode() and self._memoryEmpty():
       if os.path.exists(self._filepath()):
         with open(self._filepath(), 'r') as f:
           self._data = json.load(f)
   def runStep(self, page, raiseError=True):
     self._stepCounter += 1
-    if not self._stepInLog(self._stepCounter):
+    if not self._stepInMemory(self._stepCounter):
       self._initStep(self._stepCounter)
-    self._setLog(self._stepCounter, 'typeOfPage', defaultValue=page.__class__.__name__)
-    self._setLog(self._stepCounter, 'settings', defaultValue=page.settings())
-    self._setLog(self._stepCounter, 'statistics', defaultValue=self._setStatistics(page.statistics()))
-    result = self._getLog(self._stepCounter, 'result')
+    self._setMemory(self._stepCounter, 'typeOfPage', defaultValue=page.__class__.__name__)
+    self._setMemory(self._stepCounter, 'settings', defaultValue=page.settings())
+    self._setStat(self._stepCounter, 'statistics', defaultValue=self._setStatistics(page.statistics()))
+    result = self._getMemory(self._stepCounter, 'result')
     if result:
       return result
     if raiseError:
@@ -84,15 +84,15 @@ class ManuscriptMemory():
   def saveCurrentStep(self, result):
     return self.save(self._stepCounter, result)
   def save(self, step, result):
-    if not step or not self._stepInLog(step) or self._getLog(step, 'result') is not None:
-      return self._getLog(step, 'result')
+    if not step or not self._stepInMemory(step) or self._getMemory(step, 'result') is not None:
+      return self._getMemory(step, 'result')
     result['timestamp'] = self._currentTimestamp()
-    self._setLog(step, 'result', defaultValue=result)
+    self._setMemory(step, 'result', defaultValue=result)
     with open(self._filepath(), 'w') as f:
       json.dump(self._data, f)
     return result
   def settings(self, step):
-    settings = self._getLog(step, 'settings')
+    settings = self._getMemory(step, 'settings')
     return settings if settings is not None else {}
 
 class ManuscriptMemories():
