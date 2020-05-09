05070fd3360107d42b5ef5b8b574c2b6df6dbc9f
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2020-05-09 21:05:39 +0200

    feature: statistics V

--- Empiric/Pages/PageQuestionnaire.py
+++ Empiric/Pages/PageQuestionnaire.py
@@ -1,7 +1,7 @@
 from xml.etree import ElementTree
 
 from Empiric.internal.Print import COLORS, Print
-from Empiric.internal.Statistics import VISUALIZATION
+from Empiric.internal.Statistics import VISUALIZATION_TYPE
 from Empiric.Pages.Page import Page
 
 class PageQuestionnaire(Page):
@@ -9,32 +9,66 @@ class PageQuestionnaire(Page):
   def _attrib(n, key, defaultValue=None):
     return n.attrib[key] if n.attrib and key in n.attrib else defaultValue
   def run(self, **settings):
-    statistics = {}
-    try:
-      if 'questions' in settings:
-        questions = ElementTree.fromstring('<root>' + settings['questions'] + '</root>')
-        for n in questions:
-          key = PageQuestionnaire._attrib(n, 'key')
-          if not key or key in statistics:
-            continue
-          s = {'key': key}
-          if n.tag == 'choice':
-            s['visualization'] = VISUALIZATION.BAR_CHART
-            s['options'] = list(map(lambda x: x.text, n))
-            statistics[key] = s
-          elif n.tag == 'slider':
-            s['visualization'] = VISUALIZATION.BOX_PLOT
-            s['min'] = PageQuestionnaire._attrib(n, 'min')
-            s['max'] = PageQuestionnaire._attrib(n, 'max')
-            s['min-label'] = PageQuestionnaire._attrib(n, 'min-label')
-            s['center-label'] = PageQuestionnaire._attrib(n, 'center-label')
-            s['max-label'] = PageQuestionnaire._attrib(n, 'max-label')
-            statistics[key] = s
-          elif n.tag == 'text':
-            s['visualization'] = VISUALIZATION.TEXT_COLLECTION
-            statistics[key] = s
-    except Exception as e:
-      Print.log('WARNING: Could not parse questionnaire')
+    statistics = None
+    if 'statistics' in settings and isinstance(settings['statistics'], str):
+      statistics = {
+        'title': settings['statistics'],
+      }
+      try:
+        substatistics = {}
+        if 'questions' in settings:
+          questions = ElementTree.fromstring('<root>' + settings['questions'] + '</root>')
+          for n in questions:
+            key = PageQuestionnaire._attrib(n, 'key')
+            if not key or key in statistics:
+              continue
+            s = {'key': key}
+            if n.tag == 'choice':
+              substatistics[key] = {
+                **s,
+                'title': PageQuestionnaire._attrib(n, 'text'),
+                'data': {
+                  'selector': f'questionnaire.{key}',
+                  'aggregateByPage': 'first',
+                },
+                'visualization': {
+                  'type': VISUALIZATION_TYPE.BAR_CHART,
+                  'options': list(map(lambda x: x.text, n)),
+                },
+              }
+            elif n.tag == 'slider':
+              substatistics[key] = {
+                **s,
+                'title': PageQuestionnaire._attrib(n, 'text'),
+                'data': {
+                  'selector': f'questionnaire.{key}',
+                  'aggregateByPage': 'first',
+                },
+                'visualization': {
+                  'type': VISUALIZATION_TYPE.BOX_PLOT,
+                  'options': list(map(lambda x: x.text, n)),
+                  'min': PageQuestionnaire._attrib(n, 'min'),
+                  'max': PageQuestionnaire._attrib(n, 'max'),
+                  'min-label': PageQuestionnaire._attrib(n, 'min-label'),
+                  'center-label': PageQuestionnaire._attrib(n, 'center-label'),
+                  'max-label': PageQuestionnaire._attrib(n, 'max-label'),
+                },
+              }
+            elif n.tag == 'text':
+              substatistics[key] = {
+                **s,
+                'title': PageQuestionnaire._attrib(n, 'text'),
+                'data': {
+                  'selector': f'questionnaire.{key}',
+                  'aggregateByPage': 'first',
+                },
+                'visualization': {
+                  'type': VISUALIZATION_TYPE.TEXT_COLLECTION,
+                },
+              }
+        statistics['substatistics'] = list(substatistics.values())
+      except Exception as e:
+        Print.log('WARNING: Could not parse questionnaire')
     return super().run(defaultStatistics=statistics, **settings)
 
 def pageQuestionnaire(m, title='', questions='', **kwargs):
