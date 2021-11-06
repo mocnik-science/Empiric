c33920d73948e8f261f0e2417ac5cf6ca51297b7
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2021-11-07 00:31:46 +0100

    pageFinal improved

--- Empiric/Pages/PageFinal.py
+++ Empiric/Pages/PageFinal.py
@@ -3,5 +3,5 @@ from Empiric.Pages.Page import Page
 class PageFinal(Page):
   pass
 
-def pageFinal(m, title='Thank you!', message='You have helped us a lot!  We highly appreciate that.', **kwargs):
+def pageFinal(m, title='Thank you for completing this questionnaire!', message='Your participation in this study helps us.  This is highly appreciated.\n\nYou may now close the browser window or tab.', **kwargs):
   return PageFinal(m, 'pageFinal.html').run(hideStatisticsByDefault=True, title=title, message=message, **kwargs)
