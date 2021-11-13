81b4bdcb37103e3f25d06971a1b64a871ff693ad
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2021-11-13 02:14:12 +0200

    feature: pageFinalComments introduced II

--- Empiric/Pages/PageFinalComments.py
+++ Empiric/Pages/PageFinalComments.py
@@ -2,9 +2,8 @@ from Empiric.Pages.PageQuestionnaire import PageQuestionnaire
 
 def pageFinalComments(m, statistics='finalComments', title='Final Comments', message='Is there anything related to this questionnaire you would like to say?', **kwargs):
   data = PageQuestionnaire(m, 'pageQuestionnaire.html').run(statistics=statistics, title=title, questions='''
-<infotext>{message}</infotext>
 <text
-  key="finalComment"
+  key="finalComments"
   text="{message}"
   rows="5"
 ></text>
