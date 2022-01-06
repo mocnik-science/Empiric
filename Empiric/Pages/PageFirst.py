987d529e4d92cecbbec24c7e35899c940049aeed
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2022-01-06 19:33:40 +0100

    pageFirst: parameters improved

--- Empiric/Pages/PageFirst.py
+++ Empiric/Pages/PageFirst.py
@@ -1,7 +1,7 @@
 from Empiric.Pages.PageFinal import pageFinal
 from Empiric.Pages.PageQuestionnaire import PageQuestionnaire
 
-def pageFirst(m, statistics='consent', title='Welcome', message='Thank you for taking the time to participate in this survey.\n\nYour participation in this survey is entirely voluntary.  You can end your participation at any time by closing this website.\n\nBy clicking “Yes, I agree” below you indicate that you are at least 18 years old, have read and understood this consent form, and agree to participate in this research study.', message2='Go to the next page by clicking the green button in the top right corner.', titleNoConsent='Thank you!', messageNoConsent='You have opted to not participate.  We wish you a nice day.\n\nYou may now close the browser window or tab.', **kwargs):
+def pageFirst(m, statistics='consent', title='Welcome', message='Thank you for taking the time to participate in this survey.\n\nYour participation in this survey is entirely voluntary.  You can end your participation at any time by closing this website.\n\nBy clicking “Yes, I agree” below you indicate that you are at least 18 years old, have read and understood this consent form, and agree to participate in this research study.', message2='Go to the next page by clicking the green button in the top right corner.', buttonYes='Yes, I agree <br/>and want to participate', buttonNo='No, I do not <br/>want to participate', titleNoConsent='Thank you!', messageNoConsent='You have opted to not participate.  We wish you a nice day.\n\nYou may now close the browser window or tab.', **kwargs):
 
   data = PageQuestionnaire(m, 'pageQuestionnaire.html').run(statistics=statistics, title=title, questions='''
 <infotext>{message}</infotext>
@@ -10,14 +10,14 @@ def pageFirst(m, statistics='consent', title='Welcome', message='Thank you for t
   required="1"
   noListItem="1"
 >
-  <option onClick="$('.nextStepButton i').addClass('pulsate')">Yes, I agree <br/>and want to participate</option>
-  <option onClick="$('.nextStepButton i').addClass('pulsate')">No, I do not <br/>want to participate</option>
+  <option onClick="$('.nextStepButton i').addClass('pulsate')">{buttonYes}</option>
+  <option onClick="$('.nextStepButton i').addClass('pulsate')">{buttonNo}</option>
 </choice>
 <gap size="3rem"></gap>
 <infotext>{message2}</infotext>
-  '''.format(message=message, message2=message2), showLogo=True, **kwargs)
+  '''.format(message=message, message2=message2, buttonYes=buttonYes, buttonNo=buttonNo), showLogo=True, **kwargs)
 
-  if data['questionnaire']['consent'] != 'Yes, I agree and want to participate':
+  if data['questionnaire']['consent'] != buttonYes.replace('<br/>', '').replace('<br>', ''):
     pageFinal(m, state='noConsent', title=titleNoConsent, message=messageNoConsent)
 
   return data
--- docs/pageFirst.md
+++ docs/pageFirst.md
@@ -17,6 +17,8 @@ The following keyword arguments can be used:
 | `title` | `String` | no | `'Welcome'` | Title to be shown |
 | `message` | `String` | no | `'Thank you for taking the time to participate in this survey.\n\nYour participation in this survey is entirely voluntary.  You can end your participation at any time by closing this website.\n\nBy clicking “Yes, I agree” below you indicate that you are at least 18 years old, have read and understood this consent form, and agree to participate in this research study.'` | Message to be shown |
 | `message2` | `String` | no | `'Go to the next page by clicking the green button in the top right corner.'` | Message to be shown after the choice for consent |
+| `buttonYes` | `String` | `'Yes, I agree <br/>and want to participate'` | Label of the button to confirm the participation |
+| `buttonNo` | `String` | `'No, I do not <br/>want to participate'` | Label of the button to decline the participation |
 | `titleNoConsent` | `String` | no | `'Thank you!'` | Title to be shown if no consent is provided |
 | `messageNoConsent` | `String` | no | `'You have opted to not participate.  We wish you a nice day.\n\nYou may now close the browser window or tab.'` | Message to be shown if no consent is provided |
 
